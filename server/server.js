404a6da17b2c8ea755991d6483daa425014e05a2
Shane Mitchell <shaneSC@gmail.com>
2020-11-26 14:01:30 +0000

    Update databse return to include ratings data for each restaurant

--- server/server.js
+++ server/server.js
@@ -8,12 +8,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//TODO: Move routes into Express router
+
 // Get all restaurants
 app.get('/api/v1/restaurants', async (req, res) => {
   try {
     const results = await db.query(
-      'SELECT * FROM restaurants ORDER BY name'
+      'SELECT restaurants.id, restaurants.name, restaurants.location, restaurants.price_range, reviews.count, reviews.avg_rating FROM restaurants LEFT JOIN (SELECT restaurant_id, COUNT(*), TRUNC(AVG(rating), 1) AS avg_rating FROM reviews GROUP BY restaurant_id) reviews ON restaurants.id = reviews.restaurant_id ORDER BY name'
     );
+
+    console.log(results.rows);
     res.status(200).json({
       status: 'success',
       results: results.rows.length,
@@ -32,7 +36,7 @@ app.get('/api/v1/restaurants', async (req, res) => {
 app.get('/api/v1/restaurants/:id', async (req, res) => {
   try {
     const restaurant = await db.query(
-      'SELECT * FROM restaurants WHERE id = $1',
+      'SELECT restaurants.id, restaurants.name, restaurants.location, restaurants.price_range, reviews.count, reviews.avg_rating FROM restaurants LEFT JOIN (SELECT restaurant_id, COUNT(*), TRUNC(AVG(rating), 1) AS avg_rating FROM reviews GROUP BY restaurant_id) reviews ON restaurants.id = reviews.restaurant_id WHERE id = $1',
       [req.params.id]
     );
     const reviews = await db.query(
